0a3f78685959b9752c672dd769eb2e6072933617
samuelru3 <samuelruebel@gmail.com>
2020-10-16 10:31:22 +0200

    first commit

---
+++ wuerfel.js
@@ -0,0 +1,54 @@
+function wurf() {
+    let eins = 0;
+    let zwei = 0;
+    let drei = 0;
+    let vier = 0;
+    let fünf = 0;
+    let sechs = 0;
+    var anzahl = document.getElementById("anzahl").value;
+    for (let index = 1; index <= anzahl; index++) {
+
+
+
+        let wuerfel = Math.floor(Math.random() * 6) + 1;
+        switch (wuerfel) {
+            case 1:
+                eins++;
+                break;
+
+            case 2:
+                zwei++;
+                break;
+            case 3:
+                drei++;
+                break;
+            case 4:
+                vier++;
+                break;
+            case 5:
+                fünf++;
+                break;
+            case 6:
+                sechs++;
+                break;
+            default:
+                break;
+        }
+    }
+    document.getElementById("1").innerHTML = eins;
+    document.getElementById("2").innerHTML = zwei;
+    document.getElementById("3").innerHTML = drei;
+    document.getElementById("4").innerHTML = vier;
+    document.getElementById("5").innerHTML = fünf;
+    document.getElementById("6").innerHTML = sechs;
+    // let gesamt=eins+zwei+drei+vier+fünf+sechs;
+    // console.log('Einser: ' + eins);
+    // console.log('Zweier: ' + zwei);
+    // console.log('Dreier: ' + drei);
+    // console.log('Vierer: ' + vier);
+    // console.log('Fünfer: ' + fünf);
+    // console.log('Sechser: ' + sechs);
+    // console.log('Gesamt: ' + gesamt);
+
+
+}
